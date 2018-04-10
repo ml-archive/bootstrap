8daf3524d563f7ebd06798070b01be42909c35b1
Martin Lasek <martinlasek@hotmail.de>
2018-04-10 19:02:06 +0200

    Added Source folder and Tests structure

---
+++ Tests/BootstrapTests/BootstrapTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import Bootstrap
+
+final class BootstrapTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(true, true)
+    }
+
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
