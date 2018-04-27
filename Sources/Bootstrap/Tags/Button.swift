119ffc42c2024abe6d696dcb33faf552631a0525
Martin Lasek <martinlasek@hotmail.de>
2018-04-27 16:55:30 +0200

    refactor for readability / fix style restriction

--- Sources/Bootstrap/Tags/Button.swift
+++ Sources/Bootstrap/Tags/Button.swift
@@ -16,53 +16,59 @@ public final class ButtonTag: TagRenderer {
 
     public func render(tag: TagContext) throws -> EventLoopFuture<TemplateData> {
         var style = "primary"
-        var classes: String?
-        var attributes: String?
+        var classes = ""
+        var attributes = ""
 
         if tag.parameters.count > 0 {
             guard let param = tag.parameters[0].string else {
-                throw tag.error(reason: "Wrong type given (expected a string): \(type(of: tag.parameters[0]))")
+                throw tag.error(
+                    reason: "Wrong type given (expected a string): \(type(of: tag.parameters[0]))"
+                )
             }
 
-            if param.count > 0 {
+            if !param.isEmpty {
                 style = param
             }
         }
 
         if tag.parameters.count > 1 {
             guard let param = tag.parameters[1].string else {
-                throw tag.error(reason: "Wrong type given (expected a string): \(type(of: tag.parameters[0]))")
+                throw tag.error(
+                    reason: "Wrong type given (expected a string): \(type(of: tag.parameters[1]))"
+                )
             }
 
-            if param.count > 0 {
+            if !param.isEmpty {
                 classes = param
             }
         }
 
         if tag.parameters.count > 2 {
             guard let param = tag.parameters[2].string else {
-                throw tag.error(reason: "Wrong type given (expected a string): \(type(of: tag.parameters[0]))")
+                throw tag.error(
+                    reason: "Wrong type given (expected a string): \(type(of: tag.parameters[2]))"
+                )
             }
 
-            if param.count > 0 {
+            if !param.isEmpty {
                 attributes = param
             }
         }
 
-        guard let parsedType = Keys(rawValue: style) else {
+        guard let parsedStyle = Keys(rawValue: style) else {
             throw tag.error(reason: "Wrong argument given: \(style)")
         }
-
-        // no required parameter
-        // 1: optional-style, 2. optional-classes 3. optional-attributes
-        // body: unescaped html
 
         guard let body = tag.body else {
             throw tag.error(reason: "Wrong body given: \(String(describing: tag.body))")
         }
 
-        return tag.serializer.serialize(ast: body).map(to: TemplateData.self) { er in
-            let button = "<button type=\"button\" class=\"btn btn-\(style) \(classes ?? "")\" \(attributes ?? "")>\(String(data: er.data, encoding: .utf8) ?? "")</button>"
+        return tag.serializer.serialize(ast: body).map(to: TemplateData.self) { body in
+
+            let c = "btn btn-\(parsedStyle) \(classes)"
+            let b = String(data: body.data, encoding: .utf8) ?? ""
+
+            let button = "<button type='button' class='\(c)' \(attributes)>\(b)</button>"
             return .string(button)
         }
     }
