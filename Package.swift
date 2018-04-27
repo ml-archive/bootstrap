9cfa90bb92814fbbd4efa0c917e67fc49a9f0946
Martin Lasek <martinlasek@hotmail.de>
2018-04-27 10:29:24 +0200

    Fix package name

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Boostrap",
+    name: "Bootstrap",
     products: [
         .library(name: "Bootstrap", targets: ["Bootstrap"])
     ],
