e9744508948e8dd6f0dfd7cfc0ae330ef89c8086
Martin Lasek <martinlasek@hotmail.de>
2018-04-27 16:02:00 +0200

    First successful LeafTag of a Bootstrap Button :tada:

--- Sources/Bootstrap/Bootstrap.swift
+++ Sources/Bootstrap/Bootstrap.swift
@@ -1,1 +1,22 @@
+import Vapor
+import Leaf
 
+public final class BootstrapProvider: Provider {
+    public static let repositoryName = "bootstrap"
+
+    public func register(_ services: inout Services) throws {
+        try services.register(LeafProvider())
+    }
+
+    public func didBoot(_ container: Container) throws -> EventLoopFuture<Void> {
+        return .done(on: container)
+    }
+}
+
+extension BootstrapProvider {
+    public static var tags: [String: TagRenderer] {
+        return [
+            "bs:button": ButtonTag()
+        ]
+    }
+}
--- Sources/Bootstrap/Tags/Button.swift
+++ Sources/Bootstrap/Tags/Button.swift
@@ -1,1 +1,69 @@
+import Leaf
+import TemplateKit
 
+public final class ButtonTag: TagRenderer {
+    enum Keys: String {
+        case primary = "primary"
+        case secondary = "secondary"
+        case success = "success"
+        case danger = "danger"
+        case warning = "warning"
+        case info = "info"
+        case light = "light"
+        case dark = "dark"
+        case link = "link"
+    }
+
+    public func render(tag: TagContext) throws -> EventLoopFuture<TemplateData> {
+        var style = "primary"
+        var classes: String?
+        var attributes: String?
+
+        if tag.parameters.count > 0 {
+            guard let param = tag.parameters[0].string else {
+                throw tag.error(reason: "Wrong type given (expected a string): \(type(of: tag.parameters[0]))")
+            }
+
+            if param.count > 0 {
+                style = param
+            }
+        }
+
+        if tag.parameters.count > 1 {
+            guard let param = tag.parameters[1].string else {
+                throw tag.error(reason: "Wrong type given (expected a string): \(type(of: tag.parameters[0]))")
+            }
+
+            if param.count > 0 {
+                classes = param
+            }
+        }
+
+        if tag.parameters.count > 2 {
+            guard let param = tag.parameters[2].string else {
+                throw tag.error(reason: "Wrong type given (expected a string): \(type(of: tag.parameters[0]))")
+            }
+
+            if param.count > 0 {
+                attributes = param
+            }
+        }
+
+        guard let parsedType = Keys(rawValue: style) else {
+            throw tag.error(reason: "Wrong argument given: \(style)")
+        }
+
+        // no required parameter
+        // 1: optional-style, 2. optional-classes 3. optional-attributes
+        // body: unescaped html
+
+        guard let body = tag.body else {
+            throw tag.error(reason: "Wrong body given: \(String(describing: tag.body))")
+        }
+
+        return tag.serializer.serialize(ast: body).map(to: TemplateData.self) { er in
+            let button = "<button type=\"button\" class=\"btn btn-\(style) \(classes ?? "")\" \(attributes ?? "")>\(String(data: er.data, encoding: .utf8) ?? "")</button>"
+            return .string(button)
+        }
+    }
+}
