f8efb21490ea5a7e141d68b9ea1b186d05227d7b
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2019-02-08 14:04:19 +0100

    Improve comment

--- Sources/Bootstrap/Bootstrap.swift
+++ Sources/Bootstrap/Bootstrap.swift
@@ -42,7 +42,7 @@ extension Array {
 }
 
 extension TagContext {
-    /// Throws an error if the parameter count contains more then the supplied number `n`.
+    /// Throws an error if the parameter count is larger than the supplied number `n`.
     public func requireParameterCount(upTo n: Int) throws {
         guard parameters.count <= n else {
             throw error(reason: "Invalid parameter count: \(parameters.count)/\(n)")
