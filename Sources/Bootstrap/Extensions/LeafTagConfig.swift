4bacf806d3f4effdeacaf2cc6ae0331cbd44fa54
Daniel Alm <Daniel.Alm@ForumD.net>
2019-04-11 13:36:41 +0200

    Fix tag ordering.

--- Sources/Bootstrap/Extensions/LeafTagConfig.swift
+++ Sources/Bootstrap/Extensions/LeafTagConfig.swift
@@ -10,16 +10,16 @@ extension LeafTagConfig {
             "bs:button": ButtonTag(),
             "bs:buttonGroup": ButtonGroupTag(),
             "bs:buttonToolbar": ButtonToolbarTag(),
+            "bs:card": CardTag(),
+            "bs:card:body": CardBodyTag(),
+            "bs:card:footer": CardFooterTag(),
+            "bs:card:header": CardHeaderTag(),
+            "bs:card:outer": CardOuterTag(),
             "bs:formCheckbox": FormCheckbox(),
             "bs:formFile": FormFile(),
             "bs:formRadio": FormRadio(),
             "bs:input": InputTag(),
             "bs:textArea": TextAreaTag(),
-            "bs:card": CardTag(),
-            "bs:card:outer": CardOuterTag(),
-            "bs:card:header": CardHeaderTag(),
-            "bs:card:body": CardBodyTag(),
-            "bs:card:footer": CardFooterTag(),
         ])
     }
 }
